2f8d42dd384f790c792e742e766ba21133a766b9
fahad <fjkiani@icloud.com>
2021-03-09 02:50:07 -0500

    commit

---
+++ functions/products.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const Airtable = require('airtable-node')
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('app8KYi4JU3zJeZiJ')
+  .table('takeout')
+
+exports.handler = async (event, context, cb) => {
+  const { id } = event.queryStringParameters
+  if (id) {
+    try {
+      const product = await airtable.retrieve(id)
+      if (product.error) {
+        return {
+          statusCode: 404,
+          body: `No product with id: ${id}`,
+        }
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product),
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: `Server Error`,
+      }
+    }
+  }
+    
+  try {
+    const {records} = await airtable.list({
+    maxRecords: 200,
+    })
+    // console.log(records)
+    const takeout  = records.map((product) => {
+      const { id } = product
+      const { title, image, price, description, category, spice, featured, colors } = product.fields
+      const url = image[0].url
+      return { id, title, url, price, description, category, spice, featured, colors }
+      
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify(takeout),
+    
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Server Error",
+    }
+  }
+}
